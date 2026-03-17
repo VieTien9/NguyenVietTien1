1efe8ca7241d931728b21cb6abb2f2a4211d3106
Admin <Admin@ADMIN-PC>
2026-03-17 09:20:09 +0700

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using NguyenVietTien1.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    //Navigation property
+    public List<Product> Products { get; set; }
+}
---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NguyenVietTien1.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
